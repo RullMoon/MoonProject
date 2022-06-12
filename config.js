61f44ddf3b89a312ca8769b9097f9d6972074e6b
RullMoon <107094781+RullMoon@users.noreply.github.com>
2022-06-12 11:42:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
   var sticker_name = ''
-  var sticker_author = '© All Team Bot Discussion'
+  var sticker_author = '© MoonBot'
  } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -65,8 +65,8 @@ global.author = sticker_author
 const cpack = fs.readFileSync("lib/colong.json")
 const ctickerpack = JSON.parse(cpack)
 if (ctickerpack.cpackname == '') {
-  var colong_name = '© All Team Bot Discussion'
-  var colong_author = 'Ciss 📸'
+  var colong_name = '© MoonBot'
+  var colong_author = 'Moon'
  } else {
   var colong_name = ctickerpack.cpackname
   var colong_author = ctickerpack.cauthor
@@ -83,32 +83,32 @@ global.pclg = colong_name
 global.aclg = colong_author
 //======================================//
 
-global.wm = '© All Team Bot Discussion'
+global.wm = '© MooBot'
 global.ucpn = ''
 
 //================= Url =================//
-global.linkgc = 'https://chat.whatsapp.com/CUCsW6BWfmJLJwJgPQIaKM'
-global.linkig = 'https://www.instagram.com/p/CH1A1c9J5pY/?utm_medium=copy_link'
+global.linkgc = 'https://chat.whatsapp.com/HX5ceerf4oZ3Eld0osiFqH'
+global.linkig = 'https://www.instagram.com'
 global.linkyt = ''
-global.linkfb = 'https://fb.watch/aYv0jAffAO'
-global.web = 'https://raselcomel.github.io'
-global.web1 = 'https://anu.rasell.repl.co'
+global.linkfb = 'https://fb.watch/aYv0jAffAsj'
+global.web = 'https://nekopoi.care'
+global.web1 = 'https://hentai.net'
 
 global.deslink = ''
 global.titlink = 'Folllow me on Instagram'
-global.bodlink = '© All Team Bot Discussion'
+global.bodlink = '© Moon'
 
 //========= Url Template Button =========//
 global.dtu = 'Group Bot WhatsApp'
-global.urlnya = 'https://chat.whatsapp.com/CUCsW6BWfmJLJwJgPQIaKM'
+global.urlnya = 'https://chat.whatsapp.com/HX5ceerf4oZ3Eld0osiFqH'
 
 //========= Call Template Button =========//
 global.dtc = 'Add Me'
-global.nmbrnya = '+62 822-5604-8971'
+global.nmbrnya = '+6285746030186'
 
 
 //============ Proses Bot ============//
-global.wait = 'Tunggu sebentar ya sayang'
+global.wait = 'Tunggu sebentar ya'
 global.eror = 'Maaf terjadi kesalahan, silahkan coba lagi nanti'
 
 
